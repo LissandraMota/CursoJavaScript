960951de48ea867986d6710bb45c453aefc54774
lissandramota <lissandramota1991@gmail.com>
2020-01-10 20:07:21 -0300

    anotações sobre estruturas de controle

---
+++ estruturadecontrole/while.js
@@ -0,0 +1,21 @@
+// while é uma estrutura de controle de repetição
+//é mais apropriada para quando se tem uma quantidade indeterminada de repetições
+// isso não seignifica que não se possa usar o while para repetições com quantidade determinadas de vezes, porém nesses casos é mais adequado usar o for
+//while precisa de uma expressão V ou F (enquanto a expressão fique gerando o laço até for falsa)
+
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while(opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)       //se der -1 sai do laço
+    console.log(`Opção escolhida foi ${opcao}.`)   // sómbolo da crase
+}
+
+console.log("Até a próxima")
+
+
+
