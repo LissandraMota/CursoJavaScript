6d66e84a6121679486fb179926612151d5250077
lissandramota <lissandramota1991@gmail.com>
2020-03-19 17:03:47 -0300

    anotações finais ajax

--- ajax/server.js
+++ ajax/server.js
@@ -1,12 +1,12 @@
-const bodyParser = require("body-parser")
-const express = require("express")
+const bodyParser = require('body-parser')
+const express = require('express')
 const app = express()
 
-app.use(express.static("."))
+app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-const multer = require("multer")
+const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
@@ -26,6 +26,23 @@ app.post('/upload', (req, res) => {
         }
 
         res.end("Concluído com sucesso!!")
+    })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get("/parOuImpar", (req, res) => {
+    //req.body
+    //req.query
+    //req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? "par" : "impar"
     })
 })
 
